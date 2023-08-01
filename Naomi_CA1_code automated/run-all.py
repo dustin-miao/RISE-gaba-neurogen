7a8a51d3f5440a5a3c63c087112da145cb075d23
Dustin Miao <67491820+dustin-miao@users.noreply.github.com>
2023-08-01 08:21:35 -0400

    added Naomi_CA1 automated script

---
+++ Naomi_CA1_code automated/run-all.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+import os
+from colorama import Fore
+
+def compile_and_run(program_path):
+	"""
+	Compiles and runs an instance of the 'Naomi_CA_1' program located in the 
+	'program_path' directory. 
+	"""
+
+	os.chdir(program_path)
+	print(Fore.GREEN + f'[INFO] Compiling {program_path}' + Fore.RESET)
+	os.system(f'make -f Makefile')
+	print(Fore.GREEN + f'[INFO] Running {program_path}' + Fore.RESET)
+	os.system(f'./mySim')
+	os.chdir('..')
+
+
+def process_results(results_path, results_dir):
+	"""
+	Copies results folder from 'results_path' into the results directory located
+	in 'results_dir'.
+	"""
+	print(Fore.GREEN + f'[INFO] Transferring results from {results_path}' +\
+		Fore.RESET)
+	os.system(f'cp -r {results_path} ../../{results_dir}')
+
+def process_dir(folder_path, processed_path, results_path):
+	"""
+	Processes all program folders with a name starting with 'prc_', and copies
+	the results into 'results_path'.
+	"""
+
+	print(Fore.GREEN + f'[INFO] Processing \'{folder_path}\', storing results in \'{processed_path}\'' + Fore.RESET)
+	os.chdir(folder_path)
+	programs = os.listdir()
+	for program in programs:
+		if program[0] == '.':
+			continue
+		compile_and_run(program)
+		process_results(f'\'{program}\'/\'{results_path}\'', results_path)
+		print(Fore.GREEN + f'[INFO] Moving {program} to finished folder' + Fore.RESET)
+		os.system(f'mv \'{program}\' ../../{processed_path}')
+		print(Fore.GREEN + f'[INFO] Renaming ../../{results_path}/results to ../../{results_path}/\'{program} results\'')
+		# os.system('pwd')
+		# os.system('ls')
+		# os.system('ls ..')
+		# os.system('ls ../results')
+		os.system(f'mv ../../{results_path}/results ../../{results_path}/\'{program} results\'')
+	print(Fore.GREEN + f'[INFO] Finished proceesing \'{folder_path}\', results located in directory \'{results_path}\'' + Fore.RESET)
+	os.chdir('..')
+
+if __name__ == '__main__':
+	process_dir('src/process/', 'src/finished/', 'results')
